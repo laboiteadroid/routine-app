bdab53f5bab3668f271fbe171014322e3714a4a0
laboiteadroid <54857774+laboiteadroid@users.noreply.github.com>
2025-12-10 16:24:52 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,15 @@
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open("routine-cache").then(cache => {
-    return cache.addAll(["index.html","app.js","manifest.json","icon-192.png","icon-512.png"]);
-  }));
+const CACHE_NAME = 'routine-x-cache-v1';
+const FILES_TO_CACHE = ['index.html','history.html','style.css','app.js','manifest.json','icon-192.png','icon-512.png'];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
 });
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(caches.match(evt.request).then((resp) => resp || fetch(evt.request)));
 });
